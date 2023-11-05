32f8c3c5c46c053ad94127792061f124f8c6bcfa
Rahul <rahulgulati890@gmail.com>
2023-11-06 00:59:22 +0530

    Add Navbar

---
+++ src/components/Grid.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <div>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
