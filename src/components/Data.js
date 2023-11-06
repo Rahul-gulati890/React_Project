19a343086364cdd7ef662038a2bab0780f2abae1
Rahul <rahulgulati890@gmail.com>
2023-11-06 18:47:14 +0530

    Added Data

---
+++ src/components/Data.js
@@ -0,0 +1,28 @@
+const Data = [
+    {
+        Name: "UMN",
+        Count: "1089"
+    },
+    {
+        Name: "PPS",
+        Count: "907"
+    },
+    {
+        Name: "AEP",
+        Count: "679"
+    },
+    {
+        Name: "JCC",
+        Count: "19"
+    },
+    {
+        Name: "COS",
+        Count: "876"
+    },
+    {
+        Name: "URI",
+        Count: "456"
+    },
+]
+
+export default Data;
